6a4273fbfc21a023dbed2151b92cecf018c4c237
Greg Saive <gsaive@elma.fr>
2011-07-31 22:17:14 +0200

    finalize implementation of networkObjects::ircClient unitary test

--- evias/tests/networkObjects/ircClient.hpp
+++ evias/tests/networkObjects/ircClient.hpp
@@ -30,10 +30,6 @@ namespace networkObjects {
     using evias::application::__ircReplyData;
     using evias::application::Irc;
 
-    typedef int (* hookFunction) (char*, __ircReplyData*, void*);
-
-    int catchPrivateMessage(char*, __ircReplyData*, void*);
-
     class ircClient : public unitTest
     {
         public :
@@ -46,13 +42,13 @@ namespace networkObjects {
             {
                 _host   = "irc.freenode.net";
                 _port   = 6667;
-                _nick   = "eViasBot";
+                _nick1  = "eViasBot";;
                 _user   = "GregSaive";
                 _name   = "eVias Open Development";
                 _pass   = "";
 
-                _client = new Irc;
-                _client->setQuietMode(false);
+                _client  = new Irc;
+                _client->setQuietMode(true);
                 
                 setReturnCode((int) RETURN_SUCCESS);
             }
@@ -64,14 +60,49 @@ namespace networkObjects {
 
                 _client->start(
                     (char*)(_host.c_str()), _port,
-                    (char*)(_nick.c_str()),
+                    (char*)(_nick1.c_str()),
                     (char*)(_user.c_str()),
                     (char*)(_name.c_str()),
                     (char*)(_pass.c_str())
                 );
 
                 if (! _client->isConnected()) {
-                    return setReturnCode((int) ERROR_DEVELOPMENT);
+                    return setReturnCode((int) ERROR_ENVIRONMENT);
+                }
+
+                // catch until endofmotd (connected status fulfilled)
+                _client->catchIt(Irc::MSG_ENDMOTD);
+
+                // Channel JOIN
+                string codes = string(Irc::MSG_INVALIDCHAN).append("|").append(Irc::MSG_ENDLIST);
+
+                // test channel join
+                _client->channelJoin((char*) "#gregchan");
+                _client->catchIt(codes);
+
+                if (_client->getLastTreatedCommand() != Irc::MSG_ENDLIST) {
+                    return setReturnCode((int) ERROR_TEST_DATA);
+                }
+
+                // Channel PART
+                codes = string(Irc::MSG_CHANPART).append("|")
+                        .append(Irc::MSG_NOTONCHAN).append("|")
+                        .append(Irc::MSG_INVALIDCHAN);
+
+                // test valid channel part
+                _client->channelPart((char*) "#gregchan");
+                _client->catchIt(codes);
+
+                if (_client->getLastTreatedCommand() != Irc::MSG_CHANPART) {
+                    return setReturnCode((int) ERROR_TEST_DATA);
+                }
+
+                // test "notonchan" channel part
+                _client->channelPart((char*) "#whocares");
+                _client->catchIt(codes);
+
+                if (_client->getLastTreatedCommand() != Irc::MSG_NOTONCHAN) {
+                    return setReturnCode((int) ERROR_ENVIRONMENT);
                 }
 
                 return setReturnCode((int) RETURN_SUCCESS);
@@ -82,18 +113,25 @@ namespace networkObjects {
                 if (_client != NULL)
                     delete _client;
 
+                if (_client2 != NULL)
+                    delete _client2;
+
                 return _returnCode;
             }
 
         private :
 
-            Irc*   _client;
+            Irc*    _client;
+            Irc*    _client2;
             
             string _host;
             int    _port;
-            string _nick;
+            string _nick1;
+            string _nick2;
             string _user;
+            string _user2;
             string _name;
+            string _name2;
             string _pass;
     };
 
