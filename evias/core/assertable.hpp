b91d3dcd925dccd85bd420d0f7676df6a5d73681
Greg Saive <gsaive@elma.fr>
2011-08-21 19:50:17 +0200

    improvement: change class wrongAssert, now used with a template type, convenience

--- evias/core/assertable.hpp
+++ evias/core/assertable.hpp
@@ -3,6 +3,7 @@
 
 #include <exception>
 #include <string>
+#include <sstream>
 
 #include "functional.hpp"
 
@@ -12,13 +13,34 @@ namespace core {
 
     using std::string;
 
+    // the template on the exception class
+    // allows having type informations displayed on runtime
+    template <typename _a_assertion_t>
     class wrongAssert : public std::exception
     {
+        string _info;
+
     public :
+        wrongAssert(string info)
+            : std::exception(), _info(info)
+        {
+        };
+
+        virtual ~wrongAssert() throw() {};
+
+        inline void setInfo(string info)
+            { _info = info; }
+
+        inline string getInfo() const
+            { return _info.empty() ? "The assertion is wrong." : _info; }
+
         virtual const char* what() const
             throw() // this method will never throw
         {
-            return "The assertion is wrong.";
+            string processed = getInfo();
+
+            string info = string(" Infos: '") + processed + string("'");
+            return info.c_str();
         }
     };
 
@@ -31,22 +53,23 @@ namespace core {
         assertable() {};
         virtual ~assertable() {};
 
-        static void assertEqual(__valueType, __valueType);
-        static void assertEqual(__valueType, __valueType, __callback);
-        static void assertNotEqual(__valueType, __valueType);
+        static void assertEqual(__valueType, __valueType, string = "");
+        static void assertEqual(__valueType, __valueType, __callback, string = "");
+        static void assertNotEqual(__valueType, __valueType, string = "");
 
-        static void assertBiggerThan(__valueType, __valueType);
-        static void assertSmallerThan(__valueType, __valueType);
+        static void assertBiggerThan(__valueType, __valueType, string = "");
+        static void assertSmallerThan(__valueType, __valueType, string = "");
     };
 
     // API IMPLEMENTATION
 
     template <typename __valueType>
-    void assertable<__valueType>::assertEqual(__valueType v1, __valueType v2)
+    void assertable<__valueType>::assertEqual(__valueType v1, __valueType v2, string info)
     {
         if (v1 == v2) return ;
 
-        throw (wrongAssert());
+        string msg = (info.empty() ? "assertEqual operator==(v1,v2) call" : info);
+        throw (wrongAssert<__valueType>(msg.append(" is wrong")));
     }
 
     /**
@@ -63,7 +86,7 @@ namespace core {
      * @return void
      **/
     template <typename __valueType>
-    void assertable<__valueType>::assertEqual(__valueType v1, __valueType v2, bool (*callback_fptr) (__valueType,__valueType))
+    void assertable<__valueType>::assertEqual(__valueType v1, __valueType v2, bool (*callback_fptr) (__valueType,__valueType), string info)
     {
         typedef bool (*callback_t) (__valueType, __valueType);
         typedef evias::core::callback<bool, __valueType, callback_t> callback_c;
@@ -73,31 +96,35 @@ namespace core {
         // execute callback method through functor
         if (handler->execute(v1, v2)) { delete handler; return ; }
 
-        throw (wrongAssert());
+        string msg = (info.empty() ? "assertEqual callback operator (*callback_ptr)(v1,v2) call" : info);
+        throw (wrongAssert<__valueType>(msg.append(" is wrong")));
     }
 
     template <typename __valueType>
-    void assertable<__valueType>::assertNotEqual(__valueType v1, __valueType v2)
+    void assertable<__valueType>::assertNotEqual(__valueType v1, __valueType v2, string info)
     {
         if (v1 != v2) return ;
 
-        throw (wrongAssert());
+        string msg = (info.empty() ? "assertEqual operator!=(v1,v2) call" : info);
+        throw (wrongAssert<__valueType>(msg.append(" is wrong")));
     }
 
     template <typename __valueType>
-    void assertable<__valueType>::assertBiggerThan(__valueType v1, __valueType v2)
+    void assertable<__valueType>::assertBiggerThan(__valueType v1, __valueType v2, string info)
     {
         if (v1 > v2) return ;
 
-        throw (wrongAssert());
+        string msg = (info.empty() ? "assertEqual operator>(v1,v2) call" : info);
+        throw (wrongAssert<__valueType>(msg.append(" is wrong")));
     }
 
     template <typename __valueType>
-    void assertable<__valueType>::assertSmallerThan(__valueType v1, __valueType v2)
+    void assertable<__valueType>::assertSmallerThan(__valueType v1, __valueType v2, string info)
     {
         if (v1 < v2) return ;
 
-        throw (wrongAssert());
+        string msg = (info.empty() ? "assertEqual operator<(v1,v2) call" : info);
+        throw (wrongAssert<__valueType>(msg.append(" is wrong")));
     }
     
 }; // end namespace core
